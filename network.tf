7e822646eada37b84d453bceb1198f96a9bad95e
Omer <omerap12.gmail.com>
2023-10-12 17:11:56 +0300

    Open port 443

--- network.tf
+++ network.tf
@@ -72,6 +72,14 @@ resource "aws_security_group" "allow-web-traffic" {
   vpc_id      = aws_vpc.dev-vpc.id
 
   ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "HTTP"
     from_port   = 80
     to_port     = 80
