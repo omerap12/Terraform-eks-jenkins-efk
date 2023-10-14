c4dd1f971f26fd55a1b3aa299264b2b452d6f399
Omer Aplatony <omerap12@gmail.com>
2023-10-14 13:29:02 +0300

    Added attached policy script

---
+++ attach_policy.sh
@@ -0,0 +1,9 @@
+# Create policy
+# aws iam create-policy --policy-name attach-ebs --policy-document file://iam_policy.yml
+
+# Get cluster role arm
+role_name=$(kubectl -n kube-system describe configmap aws-auth | grep role |  sed 's/.*role\///')
+echo $role_name
+
+# Attach the policy
+aws iam attach-role-policy --role-name $role_name --policy-arn  arn:aws:iam::760370564012:policy/attach-ebs
