8b1746d3815615710f3b0b92761f809509159c46
Omer Aplatony <omerap12@gmail.com>
2024-05-25 09:24:05 +0300

    Update EFS configuration

--- Terraform/efs.tf
+++ Terraform/efs.tf
@@ -12,14 +12,14 @@ module "efs" {
   # Mount targets / security group
   mount_targets = {  
     "us-east-1a" = {
-      subnet_id = resource.aws_subnet.dev1.id
+      subnet_id = aws_subnet.dev1-subnet.id
     }
     "us-east-1b" = {
-      subnet_id = resource.aws_subnet.dev2.id
+      subnet_id = aws_subnet.dev2-subnet.id
     }
   }
   security_group_description = "Example EFS security group"
-  security_group_vpc_id      = resource.aws_vpc.dev-vpc.id
+  security_group_vpc_id      = aws_vpc.dev-vpc.id
   security_group_rules = {
     vpc = {
       # relying on the defaults provdied for EFS/NFS (2049/TCP + ingress)
